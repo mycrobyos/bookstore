315687dc312bf9e1ba7e21706f25033ccdaa89d9
t776186 <daniel.baiao@santander.com.br>
2023-02-06 19:12:26 -0300

    first commit

---
+++ src/test/java/br/com/tech/ada/homework/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tech.ada.homework.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
